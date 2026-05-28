08c1a453a62b862e6749216532f4f75302e07a89
Raj Posture <rajposture16@gmail.com>
2026-05-29 00:34:14 +0530

    finalchanges

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import path from "path";
+import cors from "cors";
 
 import authRoutes from "./routes/auth.route.js";
 import productRoutes from "./routes/product.route.js";
@@ -10,34 +11,106 @@ import couponRoutes from "./routes/coupon.route.js";
 import paymentRoutes from "./routes/payment.route.js";
 import analyticsRoutes from "./routes/analytics.route.js";
 import userRoutes from "./routes/user.route.js";
+
 import { connectDB } from "./lib/db.js";
 
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 5000;
 
-const __dirname = path.resolve();
+const PORT =
+	process.env.PORT || 5000;
 
-app.use(express.json({ limit: "10mb" })); // allows you to parse the body of the request
+const __dirname =
+	path.resolve();
+
+/* CORS */
+app.use(
+	cors({
+		origin: [
+			"http://localhost:5173",
+			"https://prime-cart-one.vercel.app",
+		],
+		credentials: true,
+	})
+);
+
+/* MIDDLEWARES */
+app.use(
+	express.json({
+		limit: "10mb",
+	})
+);
+
 app.use(cookieParser());
 
-app.use("/api/auth", authRoutes);
-app.use("/api/products", productRoutes);
-app.use("/api/cart", cartRoutes);
-app.use("/api/coupons", couponRoutes);
-app.use("/api/payments", paymentRoutes);
-app.use("/api/analytics", analyticsRoutes);
-app.use("/api/users", userRoutes);
-if (process.env.NODE_ENV === "production") {
-	app.use(express.static(path.join(__dirname, "/frontend/dist")));
+/* ROUTES */
+app.use(
+	"/api/auth",
+	authRoutes
+);
+
+app.use(
+	"/api/products",
+	productRoutes
+);
+
+app.use(
+	"/api/cart",
+	cartRoutes
+);
+
+app.use(
+	"/api/coupons",
+	couponRoutes
+);
+
+app.use(
+	"/api/payments",
+	paymentRoutes
+);
+
+app.use(
+	"/api/analytics",
+	analyticsRoutes
+);
+
+app.use(
+	"/api/users",
+	userRoutes
+);
+
+/* PRODUCTION */
+if (
+	process.env.NODE_ENV ===
+	"production"
+) {
+	app.use(
+		express.static(
+			path.join(
+				__dirname,
+				"/frontend/dist"
+			)
+		)
+	);
 
 	app.get("*", (req, res) => {
-		res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+		res.sendFile(
+			path.resolve(
+				__dirname,
+				"frontend",
+				"dist",
+				"index.html"
+			)
+		);
 	});
 }
 
+/* START SERVER */
 app.listen(PORT, () => {
-	console.log("Server is running on http://localhost:" + PORT);
+	console.log(
+		`Server running on port ${PORT}`
+	);
+
 	connectDB();
 });
