49319797e0e9c1652b00c4da2cfc89245118c40c
Raj Posture <rajposture16@gmail.com>
2026-05-29 00:52:30 +0530

    final changes

--- backend/server.js
+++ backend/server.js
@@ -80,31 +80,8 @@ app.use(
 	userRoutes
 );
 
-/* PRODUCTION */
-if (
-	process.env.NODE_ENV ===
-	"production"
-) {
-	app.use(
-		express.static(
-			path.join(
-				__dirname,
-				"/frontend/dist"
-			)
-		)
-	);
 
-	app.get("*", (req, res) => {
-		res.sendFile(
-			path.resolve(
-				__dirname,
-				"frontend",
-				"dist",
-				"index.html"
-			)
-		);
-	});
-}
+
 
 /* START SERVER */
 app.listen(PORT, () => {
